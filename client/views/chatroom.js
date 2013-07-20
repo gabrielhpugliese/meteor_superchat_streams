959179a2bf038ba84d3ecca175dd6e855e24b3cf
vagrant <gabrielhpugliese@gmail.com>
2013-07-20 21:54:46 +0000

    Fixed the new line insertion. Fixed #16

--- client/views/chatroom.js
+++ client/views/chatroom.js
@@ -3,10 +3,9 @@ Template.chatroom.rendered = function () {
     // TODO: Ask meteor-talk about this unbind strange behaviour
     $('#msg').unbind('focus');
     $('#msg').focus(function () {
-    	var $self = $(this);
+    	var self = this;
         KeyboardJS.on('shift + enter', function(){
-        	var val = $self.val() + '\n';
-        	$self.val(val);
+            insertAtCaret(self, '\n');
         	return false;
        	});
 
@@ -80,6 +79,38 @@ scrollToBottom = function () {
 	        chat.scrollTop = chat.scrollHeight;
 	    } catch(err) {}
     });
+}
+
+insertAtCaret = function(txtarea, text) {
+    // got from here http://stackoverflow.com/questions/1064089/inserting-a-text-where-cursor-is-using-javascript-jquery
+    var scrollPos = txtarea.scrollTop;
+    var strPos = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? "ff" : (document.selection ? "ie" : false ) );
+    if (br == "ie") {
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart('character', -txtarea.value.length);
+        strPos = range.text.length;
+    } else if (br == "ff")
+        strPos = txtarea.selectionStart;
+
+    var front = (txtarea.value).substring(0, strPos);
+    var back = (txtarea.value).substring(strPos, txtarea.value.length);
+    txtarea.value = front + text + back;
+    strPos = strPos + text.length;
+    if (br == "ie") {
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart('character', -txtarea.value.length);
+        range.moveStart('character', strPos);
+        range.moveEnd('character', 0);
+        range.select();
+    } else if (br == "ff") {
+        txtarea.selectionStart = strPos;
+        txtarea.selectionEnd = strPos;
+        txtarea.focus();
+    }
+    txtarea.scrollTop = scrollPos;
 }
 
 Deps.autorun(function () {
