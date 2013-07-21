2028ab54b1c9fb2a8d1ada1d952a747a0e4d859a
vagrant <gabrielhpugliese@gmail.com>
2013-07-21 00:59:29 +0000

    Adding site superchat.meteor.com with the package

---
+++ superchat.meteor.com.js
@@ -0,0 +1,23 @@
+if (Meteor.isClient) {
+  new ForkMe({
+    user: 'gabrielhpugliese',
+    repo: 'meteor_superchat',
+    ribbon: {
+      color: 'red',
+      position: 'left'
+    },
+  });
+  
+WebFontConfig = {
+    google: { families: [ 'Lato:400,700:latin' ] }
+};
+(function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
+
+}
