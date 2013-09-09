d8eabcdd7a0d5abb175cff8d446a4847f19029db
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2013-09-08 23:22:20 -0300

    Changed deps_path because it does not work with spiderable

--- client/lib/deps_path.js
+++ client/lib/deps_path.js
@@ -1,18 +1,19 @@
 DepsPath = function (host) {
-    this.path = '/';
-    this.deps = new Deps.Dependency;
+    var path = '/',
+        deps = new Deps.Dependency;
 
     var get = function () {
-        if (typeof Deps === 'undefined')
+        if (typeof Deps === 'undefined') {
             return;
-        Deps.depend(this.deps);
-        return this.path;
-    }.bind(this);
+        }
+        Deps.depend(deps);
+        return path;
+    };
 
     var set = function (value) {
-        this.path = value;
-        this.deps.changed();
-    }.bind(this);
+        path = value;
+        deps.changed();
+    };
 
     innerChain = {set: set};
     innerChain.__proto__ = get.__proto__;
