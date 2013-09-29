88abfbf72ac03f76ac9bb867053f19e90ba79fe3
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2013-09-29 16:59:46 -0300

    Fixing the replace group on template helper

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -2,7 +2,7 @@ if (typeof Handlebars !== 'undefined') {
     Handlebars.registerHelper('markdownAlternate', function (options) {
         var message = options.fn(this);
 
-        message = message.replace(/^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)/, '$2').replace(/^([^\n]+)\n *(=|-){2,} *(?:\n+|$)/, '$2');
+        message = message.replace(/^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)/, '$2').replace(/^([^\n]+)\n *(=|-){2,} *(?:\n+|$)/, '$1');
         return marked(message);
     });
 }
