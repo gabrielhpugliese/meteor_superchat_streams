e6dbd8d556e3103661b2daf39b8f9185ae98531c
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2013-10-12 14:49:21 -0300

    Links now open in new tab

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,9 +1,13 @@
 if (typeof Handlebars !== 'undefined') {
     Handlebars.registerHelper('markdownAlternate', function (options) {
-        var message = options.fn(this);
+        var message = options.fn(this)
+          , markedMessage = null;
 
         message = message.replace(/^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)/, '$2').replace(/^([^\n]+)\n *(=|-){2,} *(?:\n+|$)/, '$1');
-        return marked(message);
+         
+        markedMessage = marked(message);
+        markedMessage = markedMessage.replace(/<a href/g, '<a target="_blank" href');
+        return markedMessage;
     });
 
     Handlebars.registerHelper('defaultProfilePicture', function () {
